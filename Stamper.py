4833af5eb360d6578e4dfad5960a57202d989efd
Theo Crevon <tcrevon@gmail.com>
2011-10-25 09:28:33 +0200

    Massive refactoring commit. Not yet functional Stamper class. This is a sandbox branch, for sandbox commits. :-)

--- Stamper.py
+++ Stamper.py
@@ -18,23 +18,78 @@
 import os
 import sys
 
+LANGUAGES = {
+    "c": ["c", "cc", "cpp", "h", "hh", "hpp"],
+    "c++": ["c", "cc", "cpp", "h", "hh", "hpp"],
+    "python": ["py"],
+    "perl": ["pl"],
+    "php": ["php"],
+    "elisp": ["el"],
+    "haskell": ["hl"]
+    "js": ["js"],
+    }
+
 
 class License:
     """
-
+    Class which intends to manage a license file I/O
+    operations. Can be used in order to retrieve it's
+    (license file)content, output it as various data
+    structures.
     """
+
     def __init__(self, license_file):
+        LANG_COMMENT_FAMILY = {
+            "stars": ["c", "c++", "js", "php"],
+            "sharps": ["python", "perl"],
+            "semicolon": ["elisp"]
+            }
+
+        # Tuple should always have three elems,
+        # a prefix, content comment, suffix.
+        LANG_COMMENT_STYLE = {
+            "stars": ('/*', '**', '*/'),
+            "shaprs": ('', '#', ''),
+            "semicolon": ('', ';;', ''),
+            }
+
         self.license_file = license_file
         self.content = self._getLicenseFileContent()
         self.size = self._getLicenseSize()
 
+        return comment_style
+
+
+    def format_as(self, lang="c"):
+        """
+        Method formating license content list using a given language
+        comment pattern.
+
+        lang            String : Language name to use comment pattern
+                        from.
+        """
+        comment_pattern = None
+        formatted_license = []
+
+        # Retrieving given language comment pattern
+        for key, value in self.LANG_COMMENT_FAMILY.items():
+            if lang in value:
+                comment_pattern = LANG_COMMENT_STYLE[key]
+
+        # Updating license content in order to comment it out
+        # using computed comment pattern
+        formatted_license.insert(0, comment_pattern[0] + "\n")
+        for line in self.content:
+            formatted_license.append(comment_pattern[1] + " " + line)
+        formatted_license.append(comment_pattern[2] + "\n")
+
+        return (formatted_license)
+
 
     def _getLicenseFileContent(self):
         """
-        Method returning the content of the license contained in
-        file "filename" as a list. Yet it will raise an IOError exception
-
-        path                    String : License file path
+        Retrieves the content of the license file as a list.
+        Yet it will raise an IOError exception
         """
         content = None
 
@@ -52,11 +107,12 @@ class License:
 
 
     def _getLicenseSize(self):
-        """Returns the license file size"""
+        """Returns the license content size"""
         size = 0
 
         for elem in self.content:
             size += len(elem)
+
         return size
 
 
@@ -68,75 +124,62 @@ class License:
             "size": self.size,
             }
 
-    def format_as(self, lang=""):
-        pass
-
 
 class Stamper:
     """ Class defining methods allowing to
     create a language specific commented license
     in order to apply it as a file header"""
 
-    def __init__(self, license):
+    def __init__(self):
         """Constructor"""
-        self.__name = "fileLicenseType"
-        self.license_content = license.content
-        self.license_size = license.size
-        self.__filesFuncs = {}
-        self.__filesHeaders = {}
-        self.initFileTypes()
+        pass
 
 
-    def initFileTypes(self):
-        """Method initializing the corresponding method
-        to apply on each file types"""
-        self.__filesFuncs = {
-            "c": self.cStyleComments,
-            "h": self.cStyleComments,
-            "cpp": self.cStyleComments,
-            "hh": self.cStyleComments,
-            "hpp": self.cStyleComments,
-            "py": self.pyStyleComments,
-            "pl": self.pyStyleComments,
-            }
-
-
-    def getTargetFilesList(self, path):
+    def _getFilesList(self, path):
         """
         Recursively retrieves a given path files
         as a list
 
         path                    String : path to retrieve files from.
         """
-        listedDirs = []
+        listed_elems = []
 
-        if os.path.isdir(path):
-            for root, dirs, files in os.walk(path):
-                for name in files:
-                    filename = os.path.join(root, name)
-                    listedDirs.append(filename)
-        else:
-            listedDirs.append(path)
-        return(listedDirs)
+        for root, dirs, files in os.walk(path):
+            for name in files:
+                filename = os.path.join(root, name)
+                listed_elem.append(filename)
+
+        return(listed_elems)
 
 
-    def dumpFileContentToList(self, path):
+    def _dumpFileContent(self, path):
         """
         Method dumping a file content to a list
 
         path                    String : file to dump path
         """
+        fileDump = []
+
         try:
             fd = open(path, 'r')
             fileDump = fd.readlines()
             fileDump.append("\n\n")
             fd.close()
-            return (fileDump)
         except IOError as (errno, strerror):
             print "I/O error({0}): {1}".format(errno, strerror)
 
+        return (fileDump)
 
-    def writeHeaderToFile(self, dest_filename, headerToWrite):
+
+    def _dumpFiles(self, files_list):
+        """
+
+        """
+#        for f in files_list:
+#            self.files_content.append(self._dumpFileContent(f))
+
+
+    def _writeHeaderToFile(self, dest_filename, headerToWrite):
         """
         Method adding a given license list at the begining
         of a file
@@ -147,7 +190,7 @@ class Stamper:
                                 as a given destination file header.
         """
         try:
-            fileDump =  self.dumpFileContentToList(dest_filename)
+            fileDump =  self._dumpFileContentToList(dest_filename)
             fd = open(dest_filename, 'w')
             fd.seek(0)
             fd.writelines(headerToWrite + fileDump)
@@ -156,66 +199,7 @@ class Stamper:
             print "I/O error({0}): {1}".format(errno, strerror)
 
 
-    def applyLicenseToFile(self, dest_filename):
+    def applyLicense(self, license, path):
         """
-        Adds the given/found license into a given file
-
-        dest_filename           String : candidate filename which license should
-                                be applied to.
         """
-        headerToWrite = ""
-        fileDescription = dest_filename.split('.')
-        fileType = str(fileDescription[-1])
-
-        if fileType in self.__filesFuncs:
-            if fileType in self.__filesHeaders:
-                headerToWrite = self.__filesHeaders[fileType]
-            else:
-                headerToWrite = self.__filesFuncs[fileType](self.license_content)
-                self.__filesHeaders[fileType] = headerToWrite
-            self.writeHeaderToFile(dest_filename, headerToWrite)
-
-
-    def cStyleComments(self, licenseContent):
-        """
-        Method formating a given license content list,
-        to C Style language commented output
-
-        licenseContent          List : Retrieved license content from file
-        """
-        newLicense = []
-        newLicense.insert(0, "/*\n")
-
-        for line in licenseContent:
-            newLicense.append("** " + line)
-        newLicense.append("*/\n\n")
-        return (newLicense)
-
-
-    def pyStyleComments(self, licenseContent):
-        """
-        Method formating a given license content list,
-        to Python Style language commented output
-
-        licenseContent          List : Retrieved license content from file
-        """
-        newLicense = []
-
-        for line in licenseContent:
-            newLicense.append("# " + line)
-        newLicense.append("\n\n")
-        return (newLicense)
-
-
-    def applyLicenseToFiles(self, path):
-        """
-        Applies a given/found license as
-        a header to recursively found files in path
-
-        path                    String : Path where to seek for files
-        """
-        files = self.getTargetFilesList(path)
-
-        for f in files:
-            self.applyLicenseToFile(f)
-
+        self
