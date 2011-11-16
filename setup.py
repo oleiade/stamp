b746bb593f27b6ce309937ecb343333035a92a58
Theo Crevon <tcrevon@gmail.com>
2011-11-16 16:39:44 +0100

    Adding classifiers to setup.py

--- setup.py
+++ setup.py
@@ -15,13 +15,20 @@ setup(
     url = "http://github.com/oleiade/stamp",
 
     test_suite = "nose.collector",
-    tests_require = "nose==1.1.2",
-#    tests_require = "nose==1.0.0",
+    tests_require = "nose",
 
     # Setting up executable/main functions links
     entry_points = {
         'console_scripts': [
             'stamp = stamp.main:main',
         ]
-    }
+    },
+
+    classifiers = [
+        'Development Status :: 0.0.1 - Early Alpha',
+        'Environment :: Unix-like Systems',
+        'Intended Audience :: Developers, Project managers, Sys admins',
+        'Programming Language :: Python',
+        'Operating System :: Unix-like',
+    ],
 )
