88f16ef29561f2ed6ce8ae8e945acac4b8b4e3e4
Theo Crevon <tcrevon@gmail.com>
2011-12-08 18:16:07 +0100

    Adding documentation to Stamper class. (Consists mostly in docstrings update).

--- stamp/Stamper.py
+++ stamp/Stamper.py
@@ -60,7 +60,10 @@ class Stamper:
 
         NB : Shall not be used independtly.
 
-        folder                    String : path to retrieve files from.
+        folder                  String : path to retrieve files from.
+        exclude_dotted          Bool : Wheter to ignore paths beggining
+                                with a dot while walking dirs.
+                                Ex : .git .svn .emacs
         """
         listed_dirs = []
 
@@ -84,9 +87,9 @@ class Stamper:
 
     def _get_path_elements(self, path, exclude_dotted=True):
         """
-        Recursively retrieves a given path files.
-        returns a list of tuples (file extension, file path).
-        If given path is a folder, returns get_folder_files result,
+        Recursively retrieves a path files.
+        returns a list of lists -- [file extension, file path]
+        If path is a folder, returns get_folder_files result,
         else uses the given path file.
 
         path                    String : path to retrieve files from.
@@ -111,9 +114,10 @@ class Stamper:
 
     def _dump_file_content(self, file_descriptor):
         """
-        Method dumping a file content to a list
+        Method dumping a file content to a list.
 
-        path                    String : file to dump path
+        file_descriptor         File : file descriptor to dump
+                                content from.
         """
         file_dump = []
 
@@ -130,8 +134,8 @@ class Stamper:
 
     def _get_fd_from_path(self, path, mode="r+"):
         """
-        Opens a file_descriptors from a given path, using
-        a given opening mode. Adds it to the stamper class
+        Opens a file_descriptor from a path, using
+        with given mode. Adds it to the stamper class
         file descriptor buffer.
 
         path                    String : file path to buffer a file
@@ -157,8 +161,11 @@ class Stamper:
         the paths pointed by the paths_list
         to the object file descriptor buffer.
 
-        paths_list              List :
-        mode                    string :
+        paths_list              List : paths, that should be opened
+                                as a file descriptor and putted into
+                                the stamper fd buffer.
+        mode                    string : file descriptor opening mode
+                                that should be used.
         """
         for p in paths_list:
             self._get_fd_from_path(p, mode=mode)
@@ -168,6 +175,9 @@ class Stamper:
 
     def _clear_fd_buffers(self):
         """
+        Walks through the stamper object file descriptor
+        buffer, close every found fd, and clears the
+        stack.
         """
         for fd in self.fd_buffer.values():
             try:
@@ -182,11 +192,11 @@ class Stamper:
 
     def write_header_to_file(self, file_descriptor, header):
         """
-        Method adding a given license list at the begining
+        Method adding a license content at the begining
         of a file
 
-        dest_filename           String : candidate filename which license should
-                                be applied to.
+        file_descriptor         File : open file descriptor where to write
+                                license header.
         header                  List : Formatted, license content to write
                                 as a given destination file header.
         """
@@ -210,9 +220,9 @@ class Stamper:
         """
         Applies a given license (class instance) to a given path.
 
-        lic                     License : License class instance, should have
-                                been initialized with one of your choice (apache, bsd, ...)
         path                    String : File or dir, License should apply to.
+        verbose                 Bool : Whether should display it's actions on
+                                stdout or not.
         """
         files_in_path = self._get_path_elements(path)
 
