f77a7d981c47b8db82b2543f3ba7c89b3e2721e1
Theo Crevon <tcrevon@gmail.com>
2011-12-16 17:16:48 +0100

    Refactoring apply_license() method tests. Still lacks dotted paths tests. Ref #12

--- stamp/tests/stamper_test.py
+++ stamp/tests/stamper_test.py
@@ -35,18 +35,13 @@ class TestStamper(unittest.TestCase):
         file_names = [
             "py_file.py",
             "c_file.c",
-            "php_file.php",
+            "php_file.js",
         ]
+        os.mkdir('/tmp/stamper_test')
 
         for counter, f in enumerate(file_names):
-            path = "/tmp" + "/" + STAMPER_TEST_FILES_PREFIX + f
+            path = self.test_files_dir + "/" + STAMPER_TEST_FILES_PREFIX + f
             f = open(path, 'w')
-
-            if counter == 0:
-                f.write("#!/usr/bin/env python\n\n This is a shebanged test file content")
-            else:
-                f.write("This is a test file content")
-
             f.close()
             files_paths.append(path)
 
@@ -77,6 +72,13 @@ class TestStamper(unittest.TestCase):
         return
 
 
+    def __remove_test_files(self):
+        for p in self.test_files_paths:
+            os.remove(p)
+
+        return
+
+
     def assertListsList(self, lists_list):
         if lists_list and len(lists_list) > 0:
             for t in lists_list:
@@ -85,6 +87,7 @@ class TestStamper(unittest.TestCase):
 
     def setUp(self):
         # Assigning function return value for memotechnic reasons
+        self.test_files_dir = '/tmp/stamper_test'
         self.test_files_paths = self.__generate_test_files()
         self.test_license_file_path = self.__generate_test_license_file()
 
@@ -92,12 +95,61 @@ class TestStamper(unittest.TestCase):
         self.inst = Stamper.Stamper(self.license)
 
 
-    def test_apply_license(self):
-        pass
+    def test_apply_license_on_file(self):
+        # getting the path of a file without shebang
+        raw_path = self.test_files_paths[1]
+
+        # dumping it's content in order to be able to compare
+        # it after patching
+        raw_file = open(raw_path, 'r').readlines()
+        self.inst.apply_license(raw_path)
+        licensed_file = open(raw_path, 'r').readlines()
+
+        self.assertEqual(raw_file, [])
+        self.assertIsInstance(licensed_file, list)
+        self.assertNotEqual(licensed_file, [])
+
+        # As selected test path has .c extension,
+        # check the appliedlicense is a c commented one
+        self.assertEqual(licensed_file[0:3], ['\n', '\n', '/*\n'])
+        self.assertTrue(licensed_file[3].startswith('**'))
+
+
+    def test_apply_license_on_path(self):
+        # getting the path of files meant to be patched
+        raw_paths = self.test_files_paths[1:]
+
+        # dumping the files (empty) content, applying license to
+        # the path their stored at. Retrieving their patched content.
+        raw_files_content = [open(p, 'r').readlines() for p in raw_paths]
+        self.inst.apply_license(self.test_files_dir)
+        licensed_files_content = [open(f, 'r').readlines() for f in raw_paths]
+
+
+        self.assertEqual(len(raw_files_content), len(licensed_files_content))
+        i = 0
+        # dumping it's content in order to be able to compare
+        # it after patching
+        while i < len(raw_files_content):
+            raw_file = raw_files_content[i]
+            licensed_file = licensed_files_content[i]
+            self.assertEqual(raw_file, [])
+            self.assertIsInstance(licensed_file, list)
+            self.assertNotEqual(licensed_file, [])
+
+            # As selected test path has .c extension,
+            # check the appliedlicense is a c commented one
+            self.assertEqual(licensed_file[0:3], ['\n', '\n', '/*\n'])
+            self.assertTrue(licensed_file[3].startswith('**'))
+            i += 1
 
 
     def tearDown(self):
         self.__flush_test_files()
+        self.__remove_test_files()
+
+        os.remove(self.test_license_file_path)
+        os.rmdir(self.test_files_dir)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestStamper)
 unittest.TextTestRunner(verbosity=2).run(suite)
